b854399c13d8cf493fe309a43015e5529bf2b2e9
sandeeppunmia <67103020+sandeeppunmia@users.noreply.github.com>
2022-04-02 18:39:40 +0530

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("player-movement",{
+    init:function(){
+        this.walk();
+    },
+    walk:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(
+                e.key === "ArrowUp" || e.key === "ArrowLeft" || e.key === "ArrowRight"
+            ){
+                var entity = document.querySelector("#sound2")
+                entity.components.sound.playSound();
+            }
+        })
+    }
+})
